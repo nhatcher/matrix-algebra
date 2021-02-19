84aae6f098ca12906f68bc03d98725ed502b9e3e
Nicolás Hatcher <nicoliere@gmail.com>
2021-02-19 12:23:55 +0100

    Adds bin folder

---
+++ bin/linalg.js
@@ -0,0 +1,66 @@
+
+async function linalg(getAssembly) {
+  // 256 => 16MB
+  const memory = new WebAssembly.Memory({initial: 512});
+  const env = {memory: memory};
+  const instance = await getAssembly({env});
+  
+  const malloc = instance.exports.malloc;
+  const free = instance.exports.free;
+
+  function multiply(A, B, N) {
+    const L = N * N;
+    console.assert(A.length === L);
+    const pA = malloc(L * 8);
+    const cA = new Float64Array(memory.buffer, pA, L);
+    cA.set(A);
+    const pB = malloc(L * 8);
+    const cB = new Float64Array(memory.buffer, pB, L);
+    cB.set(B);
+    const pC = malloc(L * 8);
+    const cC = new Float64Array(memory.buffer, pC, L);
+    instance.exports.multiply(pA, pB, pC, N);
+    const D = new Float64Array(L);
+    D.set(cC)
+    free(pA);
+    free(pB);
+    free(pC);
+    return D;
+  }
+
+  function determinant(A, N) {
+    const L = N * N;
+    console.assert(A.length === L);
+    const pA = malloc(L * 8);
+    const cA = new Float64Array(memory.buffer, pA, L);
+    cA.set(A);
+    const d = instance.exports.determinant(pA, N);
+    free(pA);
+    return d;
+  }
+
+  function inverse(A, N) {
+    const L = N * N;
+    console.assert(A.length === L);
+    const pA = malloc(L * 8);
+    const cA = new Float64Array(memory.buffer, pA, L);
+    cA.set(A);
+    const pIA = malloc(L * 8);
+    const cIA = new Float64Array(memory.buffer, pIA, L);
+    instance.exports.inverse(pA, pIA, N);
+    const B = new Float64Array(L);
+    B.set(cIA);
+    free(pA);
+    free(pIA);
+    return B;
+  }
+  return {
+    multiply,
+    inverse,
+    determinant
+  }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = {linalg}
+}
